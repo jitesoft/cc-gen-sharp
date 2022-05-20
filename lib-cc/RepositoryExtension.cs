b211e576c02b9bac59072cd86242c7e718cb2231
Johannes Tegnér <johannes@jitesoft.com>
2022-05-20 14:59:03 +0200

    feat(lib-cc): RepositoryExtension GetConventionalCommits.
    First naive implementation.
    
    Signed-off-by: Johannes Tegnér <johannes@jitesoft.com>

---
+++ lib-cc/RepositoryExtension.cs
@@ -0,0 +1,33 @@
+﻿using LibGit2Sharp;
+
+namespace Jitesoft.Libs.ConventionalCommits;
+
+public static class RepositoryExtension
+{
+    public static IList<Conventional> GetConventionalCommits(this Repository self, Tag? from = null, Tag? until = null, CommitSortStrategies strategy = CommitSortStrategies.Topological)
+    {
+        var fromSha = from?.Target?.Sha ?? self.Head.Tip.Sha;
+        var toSha = until?.Target?.Sha;
+        
+        var commits = self.Commits.QueryBy(new CommitFilter
+        {
+            SortBy = CommitSortStrategies.Topological,
+            IncludeReachableFrom = fromSha,
+            ExcludeReachableFrom = toSha,
+        });
+        
+        var filtered = commits.Where(c => c.IsConventional());
+        var result = new List<Conventional>();
+        foreach (var c in filtered)
+        {
+            if (c.ParseConventional(out var conventional))
+            {
+                result.Add(conventional!);
+            }
+        }
+
+        result.Reverse();
+
+        return result;
+    }
+}
