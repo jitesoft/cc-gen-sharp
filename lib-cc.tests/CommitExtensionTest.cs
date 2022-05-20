5692fefb7c916bc391d2fd2432405b9ef19d2696
Johannes Tegnér <johannes@jitesoft.com>
2022-05-20 14:25:19 +0200

    feat(lib-cc): Created initial commit parser and test.
    Including a body in this text to make sure it's around!
    
    Signed-off-by: Johannes Tegnér <johannes@jitesoft.com>

---
+++ lib-cc.tests/CommitExtensionTest.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Jitesoft.Libs.ConventionalCommits;
+using LibGit2Sharp;
+using Moq;
+using Xunit;
+
+namespace lib_cc.tests;
+
+public class CommitExtensionTest
+{
+    [Fact]
+    public void TestConventionalSuccess()
+    {
+        var commitMessage = @"feat(test): this is the header
+
+This is the body
+Which is multi line!";
+
+        var mock = new Mock<Commit>();
+        mock.SetupGet(x => x.Message).Returns(commitMessage);
+
+        var commit = mock.Object;
+        
+        Assert.True(commit.IsConventional());
+
+        var success = commit.ParseConventional(out var result);
+        Assert.True(success);
+        
+        Assert.Equal("feat", result.Type);
+        Assert.Equal("test", result.SubType);
+        Assert.Equal("this is the header", result.Header);
+        Assert.Equal(@"This is the body
+Which is multi line!", result.Body);
+    }
+    
+    [Fact]
+    public void TestConventionalSuccessNoSubType()
+    {
+        var commitMessage = @"feat: this is the header
+
+This is the body
+Which is multi line!";
+
+
+        var mock = new Mock<Commit>();
+        mock.SetupGet(x => x.Message).Returns(commitMessage);
+
+        var commit = mock.Object;
+        
+        Assert.True(commit.IsConventional());
+        
+        var success = commit.ParseConventional(out var result);
+        
+        Assert.True(success);
+        Assert.Equal("feat", result.Type);
+        Assert.Equal("", result.SubType);
+        Assert.Equal("this is the header", result.Header);
+        Assert.Equal(@"This is the body
+Which is multi line!", result.Body);
+    }
+    
+    [Fact]
+    public void TestConventionalSuccessNoBody()
+    {
+        var commitMessage = @"feat: this is the header";
+
+
+        var mock = new Mock<Commit>();
+        mock.SetupGet(x => x.Message).Returns(commitMessage);
+
+        var commit = mock.Object;
+        
+        Assert.True(commit.IsConventional());
+        
+        var success = commit.ParseConventional(out var result);
+        
+        Assert.True(success);
+        
+        Assert.Equal("feat", result.Type);
+        Assert.Equal("", result.SubType);
+        Assert.Equal("this is the header", result.Header);
+        Assert.Equal("", result.Body);
+    }
+    
+    [Fact]
+    public void TestIsNotConventional()
+    {        
+        var commitMessage = @"This is not conventional
+
+This is the body
+Which is multi line!";
+        
+        var mock = new Mock<Commit>();
+        mock.SetupGet(x => x.Message).Returns(commitMessage);
+
+        var commit = mock.Object;
+        
+        Assert.False(commit.IsConventional());
+        var success = commit.ParseConventional(out var result);
+        
+        Assert.False(success);
+        Assert.Null(result);
+        
+    }
+}
---
+++ lib-cc/CommitExtension.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+using LibGit2Sharp;
+
+namespace Jitesoft.Libs.ConventionalCommits;
+
+public static class CommitExtension
+{
+    private static readonly Regex ConventionalParseRegex = new Regex("^(\\w+)[(]?(\\w+)?[)]?[:]\\s(.*)([\\n]+(?s)(.*))?");
+
+    public static bool IsConventional(this Commit self)
+    {
+        return ConventionalParseRegex.IsMatch(self.Message);
+    }
+    
+    public static bool ParseConventional(this Commit self, out Conventional? conventional)
+    {
+        if (!self.IsConventional())
+        {
+            conventional = null;
+            return false;
+        }
+
+        try
+        {
+            var parsed = ConventionalParseRegex.Matches(self.Message);
+            conventional = new Conventional
+            {
+                Type = parsed[0].Groups[1].Value.Trim(),
+                SubType = parsed[0].Groups[2].Value.Trim(),
+                Header = parsed[0].Groups[3].Value.Trim(),
+                Body = parsed[0].Groups[4].Value.Trim(),
+            };
+        }
+        catch
+        {
+            conventional = null;
+            return false;
+        }
+
+        return true;
+    }
+}
